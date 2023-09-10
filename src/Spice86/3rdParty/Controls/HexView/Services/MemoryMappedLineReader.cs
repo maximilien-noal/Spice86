16dbe2d7e42be87caedc773313be65d9e9f56fbc
Maximilien Noal <noal.maximilien@gmail.com>
2023-09-10 13:48:14 +0200

    WIP custom 3rd part UI HexView
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86/3rdParty/Controls/HexView/Services/MemoryMappedLineReader.cs
@@ -0,0 +1,27 @@
+﻿namespace Spice86._3rdParty.Controls.HexView.Services;
+
+using Spice86._3rdParty.Controls.HexView.Models;
+
+public class MemoryMappedLineReader : ILineReader {
+    private readonly Memory<byte> _memory;
+
+    public MemoryMappedLineReader(Memory<byte> memory) {
+        _memory = memory;
+    }
+
+    public byte[] GetLine(long lineNumber, int width) {
+        var bytes = new byte[width];
+        var offset = lineNumber * width;
+        
+        for (int i = 0; i < width; i++) {
+            long position = offset + i;
+            if (position > _memory.Length) {
+                break;
+            }
+            Range range = new((int)position, (int)position);
+            bytes[i] = _memory[range].ToArray()[0];
+        }
+
+        return bytes;
+    }
+}
--- src/Spice86/UserControls/HexSingleView.axaml.cs
+++ src/Spice86/UserControls/HexSingleView.axaml.cs
@@ -1,11 +1,36 @@
+namespace Spice86.UserControls;
+
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
-namespace Spice86.UserControls; 
+using Spice86._3rdParty.Controls.HexView.Models;
+using Spice86._3rdParty.Controls.HexView.Services;
 
 public partial class HexSingleView : UserControl {
-    public HexSingleView() {
-        InitializeComponent();
+    private ILineReader? _lineReader;
+    private IHexFormatter? _hexFormatter1;
+
+    public HexSingleView() => InitializeComponent();
+
+    protected override void OnDataContextChanged(EventArgs e) {
+        _lineReader = new MemoryMappedLineReader(DataSource);
+        _hexFormatter1 = new HexFormatter(DataSource.Length);
+        HexViewControl1.HexFormatter = _hexFormatter1;
+        HexViewControl1.InvalidateScrollable();
+        base.OnDataContextChanged(e);
+    }
+
+    /// <summary>
+    /// Defines a <see cref="StyledProperty{TValue}"/> for the <see cref="DataSource"/> property.
+    /// </summary>
+    public static readonly StyledProperty<Memory<byte>> DataSourceProperty =
+        AvaloniaProperty.Register<PaletteUserControl, Memory<byte>>(nameof(DataSource));
+
+    /// <summary>
+    /// Gets or sets the source for the hexadecimal values.
+    /// </summary>
+    public Memory<byte> DataSource {
+        get { return GetValue(DataSourceProperty); }
+        set { SetValue(DataSourceProperty, value); }
     }
 }
